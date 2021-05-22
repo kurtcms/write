e1a96896bc514cf33ebb418a6c920d5b90c61ed6
Kurt C. M. See <kurtcms@kurt-macbook-air.local>
2021-05-23 00:42:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Write: A Blank Canvas WordPress Theme for Thoughts of All Sorts
+
+<kbd>![alt text](https://kurtcms.org/git/write/write-screenshot.png)</kbd>
+
+Theme Name: Write  
+Theme URI: https://github.com/kurtcms/write  
+Author: Kurt C. M. See  
+Author URL: https://kurtcms.org  
+Description: Building upon the awesome starter theme of [_s](https://github.com/Automattic/_s) and the wondrous [Bootstrap 5](https://github.com/twbs/bootstrap) CSS framework, Write is a blank canvas WordPress theme for thoughts of all sorts.  
+Version: 10.0  
+Tested up to: 5.6  
+Requires PHP: 5.6  
+License: GNU General Public License v2 or later  
+License URL: LICENSE  
+Text Domain: write  
+Tags: two-columns, right-sidebar, custom-menu, custom-logo, featured-image-header, featured-images, flexible-header, rtl-language-support, sticky-post, threaded-comments, translation-ready  
+
+Write WordPress Theme, (C) 2021 Kurt C. M. See  
+Write is distributed under the terms of the GNU GPL.  
+  
+## Installation
+1. Download this repository as a ZIP.
+2. Login to your WordPress Admin dashboard and navigate to 'Themes' under 'Appearance'.
+3. Choose 'Add New' and then 'Upload Theme'.
+4. Select the download ZIP and choose 'Install Now'.
---
+++ functions.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * write functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package write
+ */
+
+if ( ! defined( 'WRITE_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( 'WRITE_VERSION', '10.0' );
+}
+
+if ( ! function_exists( 'write_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function write_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on write, use a find and replace
+		 * to change 'write' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'write', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'write' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'write_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'write_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function write_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'write_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'write_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function write_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'write' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'write' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'write_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function write_scripts() {
+	wp_enqueue_style( 'write-style', get_stylesheet_uri(), array(), WRITE_VERSION );
+	wp_style_add_data( 'write-style', 'rtl', 'replace' );
+
+	/*wp_enqueue_script( 'write-navigation', get_template_directory_uri() . '/js/navigation.js', array(), WRITE_VERSION, true );*/
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'write_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
+/**
+ * Load WooCommerce compatibility file.
+ */
+if ( class_exists( 'WooCommerce' ) ) {
+	require get_template_directory() . '/inc/woocommerce.php';
+}
